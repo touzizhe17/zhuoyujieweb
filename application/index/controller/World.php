728ffb26ddfad6bcd9053c7b44ff4e32c8626712
kurongdashi <1970664102@qq.com>
2018-03-26 16:44:20 +0800

    抽离了第二个模板

--- application/index/controller/World.php
+++ application/index/controller/World.php
@@ -8,10 +8,22 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Request;
 
 class World extends Controller{
 
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+        $this->view->engine->layout('layout/layout_world');
+    }
+
     public function index(){
+
+        return $this->fetch();
+    }
+    public function zuoping(){
+
         return $this->fetch();
     }
 }
